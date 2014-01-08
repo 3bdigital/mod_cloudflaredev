e15bdff098ab6b63fe9bad6901d0d38229c36ff7
Jordan Worner <jordan.worner@gmail.com>
2014-01-08 13:44:14 +0000

    removed unused helper

--- mod_cloudflaredev.php
+++ mod_cloudflaredev.php
@@ -2,8 +2,6 @@
 // no direct access
 defined('_JEXEC') or die;
 
-require_once dirname(__FILE__).'/helper.php';
-
 $moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
 
 require JModuleHelper::getLayoutPath('mod_cloudflaredev', $params->get('layout', 'default'));
